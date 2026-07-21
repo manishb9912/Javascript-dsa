8b0f76b9e058796d4d21627f5b3ad52adbae05da
manish batgiri <manishbatgiri9912@gmail.com>
2026-07-21 13:16:08 +0530

    Added push and pop method in linkedlist

--- dataStructure/linkedList/LinkedList.js
+++ dataStructure/linkedList/LinkedList.js
@@ -26,9 +26,32 @@ class LinkedList{
         }
         return this;
     }
+    pop() {
+        if(!this.head) return undefined;
+        let temp =this.head;
+        let pre = this.head;
+        while(temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+        if(this.length == 0) {
+            this.head = null;
+            this.tail = null;
+        }
+        return temp;
+    }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.push(4);
 console.log(myLinkedList);
-
+console.log("----------------");
+myLinkedList.pop()
+console.log(myLinkedList);
+console.log("----------------");
+myLinkedList.pop()
+console.log(myLinkedList);
+console.log("----------------");
