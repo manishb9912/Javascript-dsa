31d480a245a36b61456593f717690bc6fc8e66c4
Manish <manishbatgiri9912@gmail.com>
2026-08-19 19:52:37 +0530

    Added gethead gettail unshift shift and get method

--- dataStructure/linkedList/LinkedList.js
+++ dataStructure/linkedList/LinkedList.js
@@ -13,6 +13,58 @@ class LinkedList{
         this.length = 1;
     }
 
+    print() {
+        let temp = this.head;
+        while(temp != null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if(!this.head) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: ", this.head.value);
+        }
+    }
+
+    getTail() {
+        if(!this.tail) {
+            console.log("Tail: ", null);
+        } else {
+            console.log("Tail: ", this.tail.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: ", this.length);
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    get(index) {
+        if(index < 0 || index >= this.length) return undefined;
+        let temp = this.head;
+        for(let i=0;i < index;i++) {
+            temp=temp.next;
+        }
+        return temp;
+    }
+
+    set(index, value) {
+       let temp = get(index);
+       if(temp) {
+            temp.value = value;
+            return true;
+       }
+       return false;
+    }
+
     push(value) {
         const newNode = new Node(value);
         if(!this.head) {
@@ -23,35 +75,60 @@ class LinkedList{
             this.tail = newNode;
         }
         this.length++;
-        return true;
+        return this;
     }
 
     pop() {
-        if(!this.head) {
-            return null;
-        }
+        if(this.length === 0) return undefined;
         let temp = this.head;
         let pre = temp;
 
-        while(temp.next != null) {
+        while(temp.next) {
             pre = temp;
             temp = temp.next;
         }
         this.tail = pre;
         this.tail.next = null;
+        this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
         return temp;
     }
-        print() {
-            let temp = this.head;
-            while(temp.next != null) {
-                console.log(temp);
-                temp = temp.next;
-            }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
         }
+        this.length++;
+        return this;
     }
+
+    shift() {
+        if(this.length === 0) return undefined;
+        let temp = this.head;
+        this.head = this.head.next;
+        this.length--;
+        if(this.length == 0) {
+            this.tail = null;
+        }
+        temp.next = null;
+        return temp;
+    }
+}
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.push(5);
 myLinkedList.push(8);
-myLinkedList.pop();
+myLinkedList.unshift(4);
 myLinkedList.print();
+myLinkedList.getHead();
+myLinkedList.getTail();
+console.log(myLinkedList.get(2).value);
+console.log(myLinkedList.get(-1).value);
