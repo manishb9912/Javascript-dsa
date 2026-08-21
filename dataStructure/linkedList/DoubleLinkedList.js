7bf0c4a3c8c7524773f7d320adcd05156f1256fc
Manish <manishbatgiri9912@gmail.com>
2026-08-21 13:22:46 +0530

    Added push pop unshift and shift method

--- dataStructure/linkedList/DoubleLinkedList.js
+++ dataStructure/linkedList/DoubleLinkedList.js
@@ -5,19 +5,86 @@ class Node {
         this.prev = null;
     }
 }
-class DoublyLinkedList{
+
+class DoubleLinkedList {
     constructor(value) {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = newNode;
-        this.length = 1;
+        this.lengtjh = 1;
     }
 
     printList() {
         let temp = this.head;
-        while(temp) {
+        while(temp != null) {
             console.log(temp.value);
             temp = temp.next;
         }
     }
+
+    push(value) {
+        const newNode = new Node(value);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if(this.length === 0) return undefined;
+        let temp = this.tail;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+            temp.prev = null;
+        }
+        this.length--;
+        return temp;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        if(this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    shift() {
+        if(this.length === 0) return null;
+        let temp = this.head;
+        if(this.length === 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.prev = null;
+            temp.next = null;
+        }
+        this.length--;
+        return temp;
+    }
 }
+
+const myDoubleLinkedlist = new DoubleLinkedList(7);
+myDoubleLinkedlist.push(4);
+myDoubleLinkedlist.push(5);
+myDoubleLinkedlist.push(8);
+myDoubleLinkedlist.pop();
+myDoubleLinkedlist.unshift(10);
+console.log(myDoubleLinkedlist.printList());
